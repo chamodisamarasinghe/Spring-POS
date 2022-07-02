303b85b900d24d15712913c8b3f8a209ab026de0
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-07-02 22:58:08 +0530

    Updated CustomerDTO

--- Back_End/src/main/java/lk/ijse/spring/dto/CustomerDTO.java
+++ Back_End/src/main/java/lk/ijse/spring/dto/CustomerDTO.java
@@ -1,4 +1,17 @@
 package lk.ijse.spring.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class CustomerDTO {
+    private String id;
+    private String name;
+    private String address;
+    private double salary;
 }
