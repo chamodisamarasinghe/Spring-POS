9d1a221efa3d4cd2a35fbe7a95136fe80a10d3ab
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-07-02 23:01:17 +0530

    Updated OrderDTO

--- Back_End/src/main/java/lk/ijse/spring/dto/OrderDTO.java
+++ Back_End/src/main/java/lk/ijse/spring/dto/OrderDTO.java
@@ -1,4 +1,22 @@
 package lk.ijse.spring.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class OrderDTO {
+    private String oid;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate date;
+    private CustomerDTO customer;
+    List<OrderDetailsDTO> orderDetails;
 }
