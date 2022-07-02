cef66e4954ba94ef25fca63740944399f0d9146e
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-07-02 23:00:15 +0530

    Updated OrderDetailsDTO

--- Back_End/src/main/java/lk/ijse/spring/dto/OrderDetailsDTO.java
+++ Back_End/src/main/java/lk/ijse/spring/dto/OrderDetailsDTO.java
@@ -1,4 +1,17 @@
 package lk.ijse.spring.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class OrderDetailsDTO {
+    private String oid;
+    private String itemCode;
+    private int qty;
+    private double unitPrice;
 }
